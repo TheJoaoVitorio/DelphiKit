812e2f69274156f9a3157393d8deac25e17ba0d6
TheJoaoVitorio <joaovtr999x@gmail.com>
2025-09-07 12:12:14 -0300

    apply shared styling using globals.css across all pages

--- delphi-kit/src/app/frameworks/page.tsx
+++ delphi-kit/src/app/frameworks/page.tsx
@@ -1,8 +1,28 @@
+
+
 export default function FrameworksPage() {
     return (
-        <div>
-            <h1 className="text-3xl font-bold mb-4">Frameworks</h1>
-            <p>Conteúdo da sessão Frameworks.</p>
+        <div className="pageContainer">
+            <div className="titleRed">
+                <h1 className="text-3xl font-bold mb-4">
+                Frameworks
+                </h1>
+            </div>
+
+        <p className="headerText">
+            Explore powerful Delphi frameworks that simplify your development.
+        </p>
+
+
+            {/* <div className={styles.cardGrid}>
+                {cardItems.map((item) => (
+                    <Card
+                        name={item.name}
+                        img={item.img}
+                        tags={item.tags}
+                    />
+                ))}
+            </div> */}
         </div>
     );
 }
--- delphi-kit/src/app/ui/page.tsx
+++ delphi-kit/src/app/ui/page.tsx
@@ -1,6 +1,6 @@
 import libraryService from '@/src/services/libraryService';
 import { Card } from '../../components/Card/Card';
-import styles from './page.module.css';
+// import styles from './page.module.css';
 import Badge from '@/src/components/Badge/Badge';
 
 export default function UiPage() {
@@ -8,19 +8,19 @@ export default function UiPage() {
     const cardItems = api.getLibraryData();
 
     return (
-        <div className={styles.pageContainer}>
-            <div className={styles.titleRed} >
+        <div className="pageContainer">
+            <div className="titleRed" >
                 <h1 className="text-3xl font-bold mb-4">
                     UI Library
                     <Badge text={cardItems.length.toString()} />
                 </h1>
             </div>
 
-            <p className={styles.headerText}>
+            <p className="headerText">
                 Discover a variety of UI packages and libraries for Delphi, designed to speed up the development of your applications.
             </p>
 
-            <div className={styles.cardGrid}>
+            <div className="cardGrid">
                 {cardItems.map((item) => (
                     <Card
                         name={item.name}

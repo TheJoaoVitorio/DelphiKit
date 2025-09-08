5b64942c12e077fbaafa5cc30ec83ce02ccc36c5
TheJoaoVitorio <joaovtr999x@gmail.com>
2025-09-08 14:19:06 -0300

    redirect for details page

--- delphi-kit/src/components/Card/Card.tsx
+++ delphi-kit/src/components/Card/Card.tsx
@@ -1,38 +1,41 @@
-import React from 'react';
-import styles from './Card.module.css';
+import Link from "next/link";
+import React from "react";
+import styles from "./Card.module.css";
 
 type CardProps = {
-    img: string;
-    name: string;
-    tags: string[];
+  id: number;
+  img: string;
+  name: string;
+  tags: string[];
 };
 
-export function Card({ img, name, tags }: CardProps) {
-    return (
-        <div className={styles.main}>
-            <div className={styles.card}>
-                <div className={styles.card_content}>
-                    <div
-                        className={styles.card_image}
-                        style={{ backgroundImage: `url(${img})` }}
-                    ></div>
-                </div>
-                <div className={styles.card_back}></div>
-            </div>
-
-            <div className={styles.data}>
-                <div className={styles.text}>
-                    <div className={styles.text_m}>{name}</div>
-                </div>
-            </div>
-
-            <div className={styles.btns}>
-                {tags && tags.map((tag, index) => (
-                    <div key={index} className={styles.tag}>
-                        <span>{tag}</span>
-                    </div>
-                ))}
-            </div>
+export function Card({ id, img, name, tags }: CardProps) {
+  return (
+    <Link href={`/details/${id}`} className={styles.main}>
+      <div className={styles.card}>
+        <div className={styles.card_content}>
+          <div
+            className={styles.card_image}
+            style={{ backgroundImage: `url(${img})` }}
+          ></div>
         </div>
-    );
+        <div className={styles.card_back}></div>
+      </div>
+
+      <div className={styles.data}>
+        <div className={styles.text}>
+          <div className={styles.text_m}>{name}</div>
+        </div>
+      </div>
+
+      <div className={styles.btns}>
+        {tags &&
+          tags.map((tag, index) => (
+            <div key={index} className={styles.tag}>
+              <span>{tag}</span>
+            </div>
+          ))}
+      </div>
+    </Link>
+  );
 }

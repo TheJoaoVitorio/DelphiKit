36cac8847a1923b3d8f292988a19bca4a2bf18e6
TheJoaoVitorio <joaovtr999x@gmail.com>
2025-09-12 15:55:33 -0300

    feat: add favicon.ico and update metadata for improved site branding

--- delphi-kit/src/app/metadata.ts
+++ delphi-kit/src/app/metadata.ts
@@ -3,4 +3,15 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "DelphiKit",
   description: "Link aggregator featuring Delphi repositories, visual components, and frameworks. Discover and share projects to streamline development and enhance your applications",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "16x16 32x32", type: "image/x-icon" },
+      { url: "/favicon.ico", sizes: "192x192", type: "image/png" },
+      { url: "/favicon.ico", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [
+      { url: "/favicon.ico", sizes: "180x180", type: "image/png" },
+    ],
+    shortcut: "/logo.png",
+  },
 };

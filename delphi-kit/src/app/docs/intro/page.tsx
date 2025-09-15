5bffaf173b24fee5d6bf0eedc5978d2d689300d3
TheJoaoVitorio <joaovtr999x@gmail.com>
2025-09-15 16:59:45 -0300

    docs: update documentation structure
    - Added new intro page
    - Renamed 'Introdução' to 'DelphiKit'

---
+++ delphi-kit/src/app/docs/intro/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function IntroPageDocs() {
+    return (
+        <div>
+            <h2>What is DelphiKit?</h2>
+            <p style={{ width: '80%' }} > <strong>DelphiKit</strong> is an open and collaborative platform that serves as a powerful showcase for <strong>Delphi components and frameworks</strong> . Our focus is to provide visibility to community-created tools, allowing developers to find what they need intuitively and visually.</p>
+            <p style={{ width: '80%' }} >DelphiKit's strength comes from the <strong>community</strong> . That's why we've made the contribution process incredibly simple. In this documentation, you'll learn how to add a new project to our gallery in just a few minutes, helping other developers discover amazing tools. Let's get started!</p>
+        </div>
+    )
+}
--- delphi-kit/src/app/docs/page.tsx
+++ delphi-kit/src/app/docs/page.tsx
@@ -4,16 +4,16 @@ import { SetStateAction, useState } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import styles from '../docs/page.module.css'
 
+import IntroContent from './intro/page';
 
-const IntroContent = () => <div><h2>Introdução</h2><p>Bem-vindo à nossa documentação...</p></div>;
 const ApiContent = () => <div><h2>Referência da API</h2><p>Detalhes sobre os endpoints...</p></div>;
 const GuidesContent = () => <div><h2>Guias</h2><p>Como usar nosso produto...</p></div>;
 
 
 export default function DocsPage() {
     const docsOptions = [
-        { id: 'intro', label: 'Introdução', content: <IntroContent /> },
-        { id: 'api', label: 'API', content: <ApiContent /> },
+        { id: 'intro', label: 'DelphiKit', content: <IntroContent /> },
+        { id: 'contribute', label: 'Contribute', content: <ApiContent /> },
         { id: 'guides', label: 'Guias', content: <GuidesContent /> },
     ];
 

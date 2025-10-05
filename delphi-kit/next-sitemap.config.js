6c68496483b2e0708e02db5dfc0adcaa4c996008
TheJoaoVitorio <joaovtr999x@gmail.com>
2025-10-05 17:57:49 -0300

    fix: change domain

--- delphi-kit/next-sitemap.config.js
+++ delphi-kit/next-sitemap.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://delphi-kit.vercel.app',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-    additionalSitemaps: [
-      'https://delphi-kit.vercel.app/sitemap.xml',
-    ],
-  },
-  exclude: ['/404'],
-  changefreq: 'daily',
-  priority: 0.7,
+    siteUrl: 'https://delphikit.vercel.app',
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+        ],
+        additionalSitemaps: [
+            'https://delphikit.vercel.app/sitemap.xml',
+        ],
+    },
+    exclude: ['/404'],
+    changefreq: 'daily',
+    priority: 0.7,
 }
